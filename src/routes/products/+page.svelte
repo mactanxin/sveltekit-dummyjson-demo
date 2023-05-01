dfebec81ad932cc3a1a439e9c3bde6f31e43eb54
Xin Tan <mactanxin@gmail.com>
2023-05-01 21:22:34 +0800

    🐛 fix(products): destructure products from data object in Svelte component ✨ feat(products): add fetching of products from external API in page load function

--- src/routes/products/+page.svelte
+++ src/routes/products/+page.svelte
@@ -1,7 +1,12 @@
 <script lang="ts">
   export let data;
+  $: ({ products } = data.products);
 </script>
 
 <header>
   <h1>Products</h1>
+
+  {#each products as product}
+    <p>{product.title}</p>
+  {/each}
 </header>
---
+++ src/routes/products/+page.ts
@@ -0,0 +1,10 @@
+import type { PageServerLoad } from './$types';
+
+export const load = (async ({ fetch }) => {
+  const prodRes = await fetch('https://dummyjson.com/products')
+  const products = await prodRes.json()
+
+  return {
+    products,
+  }
+}) satisfies PageServerLoad;
