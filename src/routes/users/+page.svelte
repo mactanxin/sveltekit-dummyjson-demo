1eb52225aa9d286a2176d8095f32cde7bbdd79f2
Xin Tan <mactanxin@gmail.com>
2023-05-04 13:43:27 +0800

    feat(users): dummy user list page

---
+++ src/routes/users/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  export let data: any;
+  $: ({ users } = data.users);
+</script>
+
+<h1>Users</h1>
+
+{#each users as user}
+  <p class="text-lg font-base">{user.firstName}</p>
+{/each}
