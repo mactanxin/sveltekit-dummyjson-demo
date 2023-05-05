14eecc5135c0feb5cfe55154ab505b0847d61bcd
Xin Tan <mactanxin@gmail.com>
2023-05-05 15:16:40 +0800

    ✨ feat(footer.svelte): add footer section to the application

---
+++ src/lib/components/footer.svelte
@@ -0,0 +1,19 @@
+<!-- Footer section -->
+<footer class="bg-gray-800">
+  <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
+    <div class="md:flex md:justify-center md:items-center">
+      <div class="md:flex-shrink-0">
+        <img
+          class="h-8 w-8"
+          src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
+          alt="Workflow"
+        />
+      </div>
+      <div class="mt-8 md:mt-0 md:ml-6">
+        <p class="text-center text-gray-400">
+          &copy; 2021 Product & User Management System. All rights reserved.
+        </p>
+      </div>
+    </div>
+  </div>
+</footer>
