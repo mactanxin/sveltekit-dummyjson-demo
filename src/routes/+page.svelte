fc6b5e9b2ba1e4244c53bca38388732772084eb5
Xin Tan <mactanxin@gmail.com>
2023-05-05 15:17:04 +0800

    💄 style(+page.svelte): center align h1 and p elements

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="flex justify-center items-center">Welcome to SvelteKit</h1>
+<p class="flex justify-center items-center">Dummy site</p>
