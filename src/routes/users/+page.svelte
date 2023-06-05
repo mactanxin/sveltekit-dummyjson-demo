1251167845335787c44e48d8fd1d020b303992c3
Xin Tan <mactanxin@gmail.com>
2023-06-05 16:17:16 +0800

    🔍 chore(users): add client-side pagination to users page 🐛 fix(users): fix pagination query parameters not being passed to fetch request

--- src/routes/users/+page.svelte
+++ src/routes/users/+page.svelte
@@ -1,10 +1,30 @@
 <script lang="ts">
+  import { page } from "$app/stores";
   export let data: any;
-  $: ({ users } = data.users);
+  let pageSize = 10;
+
+  $: ({ users, total, skip, limit } = data.users);
+  $: currentPage =
+    (Number($page.url.searchParams.get("pageNum")) || 0) / pageSize;
 </script>
 
 <h1>Users</h1>
+<p>this page demos how to use client side pagination in sveltekit</p>
+<p class="btn">total: {total}</p>
+<p class="btn">skip: {skip}</p>
+<p class="btn">limit: {limit}</p>
 
 {#each users as user}
   <p class="text-lg font-base">{user.firstName}</p>
 {/each}
+
+<nav>
+  {#each Array(total) as _, index}
+    <a
+      href="/users?limit={pageSize}&skip={pageSize * index}"
+      class={currentPage === index ? "text-blue-600" : "text-gray-800"}
+    >
+      {index + 1}
+    </a>
+  {/each}
+</nav>
--- src/routes/users/+page.ts
+++ src/routes/users/+page.ts
@@ -2,11 +2,13 @@
 // blog:  [here](https://tanx.in/svelte/fetching-data-in-sveltekit.html)
 import type { PageServerLoad } from './$types';
 
-export const load = (async ({ fetch }) => {
+export const load = (async ({ fetch, url }) => {
+  const limit = Number(url.searchParams.get('limit')) || 10;
+  const skip = Number(url.searchParams.get('skip')) || 0;
 
   async function fetchUsers() {
 
-    const usersRes = await fetch('https://dummyjson.com/users')
+    const usersRes = await fetch('https://dummyjson.com/users?skip=' + skip + '&limit=' + limit);
     const users = await usersRes.json()
     return users
   }
